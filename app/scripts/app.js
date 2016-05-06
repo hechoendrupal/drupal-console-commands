1c037edadd72123c5b56f15f2b300cf92691cf5e
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-05-06 13:14:09 -0700

    Add search feature and single command template.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,49 +7,9 @@
 // React.render(<Home/>, mountNode);
 
 $(function() {
-	$.ajaxSetup( { "async": false } );
+  $.ajaxSetup( { "async": false } );
 
-	var language = window.location.hash.substr(1);
+  var language = window.location.hash.substr(1);
 
-	loadCommands(language);
-
+  loadCommands(language);
 });
-
-function loadCommands(language) {
-
-  location.hash = '';
-
-  if(!language) {
-    language = 'en';
-  }
-
-  $.getJSON("languages/"+language+".json", function(data) {
-
-		console.log(data.commands);
-
-		var search = new JsSearch.Search();
-		search.addIndex('name');
-		search.addDocuments(data.commands);
-		var result = search.search('generate');
-
-		console.log(result);
-
-    // var languages = data.application.languages;
-    var namespaces = data.commands;
-    $.each(namespaces, function(namespace, commands) {
-      var templateCommands = document.getElementById('commands_template').innerHTML;
-      var commandsRender = Mustache.render(
-        templateCommands,
-        {
-          "namespace": namespace,
-          "commands": commands
-        }
-      );
-      $(commandsRender).appendTo('#commands > .namespace-list');
-    });
-  }).fail(function(jqXHR, textStatus, errorThrown) { loadCommands('en'); });
-}
-
-function search(commandName){
-	console.log(commandName);
-}
